1a400f9f1e716be1db4e931fb2e6d232b1fee3b0
Nojyto <nojusskirkevicius@gmail.com>
2021-06-16 19:28:51 +0300

    new UI

--- public/js/script.js
+++ public/js/script.js
@@ -1,3 +1,4 @@
+//const socket = io.connect("/");
 let canvas
 
 window.onload = () => {
@@ -6,12 +7,16 @@ window.onload = () => {
     canvas.freeDrawingBrush.color = "#FF2800"
 }
 
-document.getElementById("editMode").addEventListener("change", () => {
-    canvas.isDrawingMode = !document.getElementById("editMode").checked
+/*
+socket.on("update", (img) => {
+    canvas.loadFromJSON(img, () => canvas.renderAll())
 })
 
+window.addEventListener("mouseup", e => {
+    socket.emit("invoke", JSON.stringify(canvas))
+})*/
+
 function updateBrushWidth(value){
-    document.getElementById("width").innerHTML = value
     canvas.freeDrawingBrush.width = value
 }
 
@@ -19,46 +24,70 @@ function updateBrushColor(value){
     canvas.freeDrawingBrush.color = value
 }
 
-document.getElementById("clearCanvas").addEventListener("click", () => {
-    canvas.clear()
-    socket.emit("invoke", JSON.stringify(canvas))
-})
+function toggleEditMode(isKeyBind){
+    let mode = document.getElementById("editMode")
+    if(isKeyBind) mode.checked = !mode.checked
+    canvas.isDrawingMode = !mode.checked
+}
 
-document.getElementById('downloadImg').addEventListener('click', () => {
-    img = document.getElementById('downloadImg')
-    img.href = canvas.toDataURL({format: 'png', quality: 0.8})
-    img.download = 'canvas.png'
-})
-
-document.getElementById("imageLoader").onchange = (e) => {
-    var reader = new FileReader();
-        reader.onload = function (event){
-            var imgObj = new Image()
-            imgObj.src = event.target.result
+function insertImg(e){
+    console.log("yeet")
+    let reader = new FileReader()
+        reader.onload = (evt) => {
+            let imgObj = new Image()
+            imgObj.src = evt.target.result
             imgObj.onload = () => {
-                var image = new fabric.Image(imgObj)
-                image.set({
+                let img = new fabric.Image(imgObj).set({
                     angle: 0,
                     padding: 10,
                     cornersize: 10,
-                    height: 110,
-                    width: 110,
+                    height: 250,
+                    width: 250,
                 })
-                canvas.centerObject(image)
-                canvas.add(image)
-                canvas.renderAll()
+                canvas.centerObject(img).add(img).renderAll()
             }
         }
     reader.readAsDataURL(e.target.files[0])
 }
 
+function delSelectedObj(){
+    var activeObject = canvas.getActiveObject(),
+    activeGroup = canvas.getActiveGroup()
+    if(activeObject){
+        canvas.remove(activeObject)
+    }else if (activeGroup) {
+        var objectsInGroup = activeGroup.getObjects();
+        canvas.discardActiveGroup();
+        objectsInGroup.forEach((obj) => {canvas.remove(obj)})
+    }
+}
 
-const socket = io.connect("/");
-
-socket.on("update", (img) => {
-    canvas.loadFromJSON(img, () => canvas.renderAll(), (o, obj) => {/*console.log(o, obj)*/})
-})
-
-window.addEventListener("mouseup", e => {
+function clearCanvas(){
+    canvas.clear()
     socket.emit("invoke", JSON.stringify(canvas))
+}
+
+function saveCanvas(isKeyBind){
+    img = document.getElementById("downloadImg")
+    img.href = canvas.toDataURL({format: 'png', quality: 0.8})
+    img.download = "canvas.png"
+    if(isKeyBind) img.click()
+}
+
+document.addEventListener("keyup", (evt) => {
+    switch(evt.key){
+        case "E": case "e":
+            toggleEditMode(true)
+            break
+        case "Delete":
+            delSelectedObj()
+            break
+        case "C": case "c":
+            clearCanvas()
+            break
+        case "S": case "s":
+            saveCanvas(true)
+            break
+        default: break
+    }
 })
